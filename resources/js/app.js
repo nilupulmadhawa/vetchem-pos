66581b0a8cb5949c6916f5dc0db87e0eeef0ee09
PathumD46 <pathumdamunupola@gmail.com>
2022-03-05 13:45:51 +0530

    get table

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,6 +10,17 @@ import Vue from 'vue'; // if this is not work add this =>  window.Vue = require(
 import axios from 'axios';
 import VueRouter from 'vue-router';
 
+
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+
+
 Vue.use(VueRouter);
 
 import { routes } from './routes';
