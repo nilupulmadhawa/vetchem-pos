dad636a0e88a461294b90b19460b87cd39864151
PathumD46 <pathumdamunupola@gmail.com>
2022-03-06 01:34:06 +0530

    add low quantity and product list

---
+++ resources/js/components/Product-Low-Quantity.vue
@@ -0,0 +1,62 @@
+<template>
+    <!-- Main content -->
+    <section class="content">
+
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Dashboard</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Invoice</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+
+    <!-- /.content-header -->
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                 <b-table
+                    :items="items"
+                    :fields="fields"
+                    :sort-by.sync="sortBy"
+                    :sort-desc.sync="sortDesc"
+                    responsive="sm"
+                    ></b-table>
+            </div>
+        </div>
+    </div>
+
+    </section>
+</template>
+
+<script>
+    export default {
+        data() {
+      return {
+        sortBy: 'age',
+        sortDesc: false,
+        fields: [
+          { key: 'last_name', sortable: true },
+          { key: 'first_name', sortable: true },
+          { key: 'age', sortable: true },
+          { key: 'isActive', sortable: false },
+           { key: 'isActive2', sortable: false }
+        ],
+        items: [
+          { isActive: true, age: 40, first_name: 'Dickerson', last_name: 'Macdonald',isActive2: 'test' },
+          { isActive: false, age: 21, first_name: 'Larsen', last_name: 'Shaw' },
+          { isActive: false, age: 89, first_name: 'Geneva', last_name: 'Wilson' },
+          { isActive: true, age: 38, first_name: 'Jami', last_name: 'Carney' }
+        ]
+      }
+    }}
+</script>
+
