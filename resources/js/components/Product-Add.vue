f5cd67d945e74d5932bff3a9cd23315724daa49c
Samitha99 <73548027+Samitha99@users.noreply.github.com>
2022-03-05 23:29:49 +0530

    add product page develop

--- resources/js/components/Product-Add.vue
+++ resources/js/components/Product-Add.vue
@@ -21,15 +21,34 @@
 
     <!-- /.content-header -->
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Example List</div>
-
-                <div class="card-body">
-                    I'm an example List  sjgnzdbgnzbzbzsb.
-                </div>
-            </div>
+        <div class="col-md-12">
+            <div class="card p-2">
+                <form >
+                    <div class="d-flex">
+                    <div class="form-group col-md-6">
+                        <label for="Name">Name</label>
+                        <input type="text" class="form-control" id="Name" placeholder="Enter Name">
+                    </div>
+                    <div class="form-group col-md-6">
+                        <label for="Barcode">Code</label>
+                        <input type="text" class="form-control" id="Barcode" placeholder="Barcode">
+                    </div>
+                    </div>
+                    <div class="form-group ">
+                        <label for="GenerateBarcode">Barcode</label>
+                        <div class="d-flex">
+                        <input type="text" class="form-control col-md-10" id="GenerateBarcode" placeholder="Barcode">
+                        <a class="btn btn-primary col-md-2 ml-1" href="#" role="button">Generate Barcode</a>
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label for="exampleFormControlTextarea1">Example textarea</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                    </div>
+                </form>
         </div>
+    </div>
+
     </div>
 
     </section>
